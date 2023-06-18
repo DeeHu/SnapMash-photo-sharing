44a5d6e7043b3298ed43ff296251abff0542d4b6
Di Hu <dihu@dis-mbp-16.lan>
2023-06-18 14:43:54 -0700

    add docker

--- photo-sharing-client/src/AuthForm.js
+++ photo-sharing-client/src/AuthForm.js
@@ -1,98 +1,82 @@
-import React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
+import React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
 
-import { useState, useEffect } from 'react';
+import { useState } from "react";
 
 const AuthForm = ({ isSignup }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const response = await fetch(`http://localhost:5000/${isSignup ? 'signup' : 'login'}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        name,
-        email,
-        password,
-      }),
-    });
+    const response = await fetch(
+      `http://127.0.0.1:5001/${isSignup ? "signup" : "login"}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          password,
+        }),
+      }
+    );
 
     const data = await response.json();
     console.log(data);
+    if (response.ok) {
+      alert(data.message);
+    } else {
+      alert(data.message);
+    }
+
+    setEmail("");
+    setPassword("");
+    setName("");
   };
-  // const [formData, setFormData] = useState({
-  //   name: '',
-  //   email: '',
-  //   password: '',
-  // });
-
-  // const handleChange = (event) => {
-  //   setFormData({
-  //     ...formData,
-  //     [event.target.name]: event.target.value,
-  //   });
-  // };
-
-  // const handleSubmit = async (event) => {
-  //   event.preventDefault();
-  //   const response = await fetch(`http://localhost:5000/${isSignup ? 'signup' : 'login'}`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //     body: JSON.stringify(formData),
-  //   });
-  //   const data = await response.json();
-  //   console.log(data);
-  // };
 
   return (
     <Box
       component="form"
       onSubmit={handleSubmit}
       sx={{
-        '& > :not(style)': { m: 1, width: '25ch' },
+        "& > :not(style)": { m: 1, width: "25ch" },
       }}
       noValidate
       autoComplete="off"
     >
       {isSignup && (
-        <TextField 
-          id="outlined-basic" 
-          label="Name" 
-          variant="outlined" 
+        <TextField
+          id="outlined-basic"
+          label="Name"
+          variant="outlined"
           value={name}
           onChange={(e) => setName(e.target.value)}
         />
       )}
-      <TextField 
-        id="outlined-basic" 
-        label="Email" 
-        variant="outlined" 
+      <TextField
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
-      <TextField 
-        id="outlined-basic" 
-        label="Password" 
-        variant="outlined" 
-        type="password" 
+      <TextField
+        id="outlined-basic"
+        label="Password"
+        variant="outlined"
+        type="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Button 
-        variant="contained"
-        type="submit"
-      >
-        {isSignup ? 'Sign Up' : 'Log In'}
+      <Button variant="contained" type="submit">
+        {isSignup ? "Sign Up" : "Log In"}
       </Button>
     </Box>
   );
--- photo-sharing-server/app.py
+++ photo-sharing-server/app.py
@@ -1,60 +1,78 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_cors import CORS
-from flask_login import UserMixin
 from flask import request, jsonify
-from flask_login import login_user, logout_user, login_required
+import json
+import os
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'your-secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/flasksql'
-CORS(app)
+CORS(app, resources={r'/*': {'origins': '*'}})
+# CORS(app)
 
-db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 
+# Set the path to the users.json file
+users_file = os.path.join(app.root_path, "users.json")
 
-# user model
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+# Create the users.json file if it doesn't exist
+if not os.path.exists(users_file):
+    with open(users_file, "w") as f:
+        json.dump([], f)
 
+def load_users():
+    """Load the users from the users.json file."""
+    with open(users_file) as f:
+        print(f)
+        return json.load(f)
+
+def save_users(users):
+    """Save the users to the users.json file."""
+    with open(users_file, "w") as f:
+        json.dump(users, f)
+
+@app.route('/')
+def home():
+    load_users()
+    return 'Welcome to the Flask app!'
 
 @app.errorhandler(Exception)
 def handle_error(e):
     return jsonify(error=str(e)), 500
 
 
-# routes
 @app.route('/signup', methods=['POST'])
 def signup():
     data = request.get_json()
+
+    # Load existing users
+    users = load_users()
+
+    # Check if user already exists
+    if any(u['email'] == data['email'] for u in users):
+        return jsonify({'message': 'User already exists'}), 400
+
     hashed_password = bcrypt.generate_password_hash(data['password']).decode('utf-8')
-    user = User(name=data['name'], email=data['email'], password=hashed_password)
-    db.session.add(user)
-    db.session.commit()
+    new_user = {"id": len(users) + 1, "name": data['name'], "email": data['email'], "password": hashed_password}
+
+    # Add the new user to the list
+    users.append(new_user)
+    
+    # Save the updated users to the file
+    save_users(users)
+
     return jsonify({'message': 'User created successfully'}), 201
 
 @app.route('/login', methods=['POST'])
 def login():
+    # Load existing users
+    users = load_users()
     data = request.get_json()
-    user = User.query.filter_by(email=data['email']).first()
-    if user and bcrypt.check_password_hash(user.password, data['password']):
-        login_user(user)
+    user = next((u for u in users if u['email'] == data['email']), None)
+    if user and bcrypt.check_password_hash(user['password'], data['password']):
         return jsonify({'message': 'Logged in successfully'}), 200
     return jsonify({'message': 'Invalid credentials'}), 401
 
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    return jsonify({'message': 'Logged out successfully'}), 200
-
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
